2008bed6717c6da5d9796399a6be28cd51704088
Sabarish <avsabarish@gmail.com>
2025-01-31 16:02:53 +0530

    [feat]: add login page

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { Request, Response } from 'express';
+import path from 'path';
+
+const router = express.Router();
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+router.use("/user", express.static(path.join(__dirname, "../static")));
+
+router.get("/login", (req: Request, res: Response): void => {
+    const filePath = path.join(__dirname, "../static/login.html");
+    const fixedPath = filePath.replace(/^\\/, '');
+    res.sendFile(fixedPath, (err) => {
+        if (err) {
+            console.error('Error serving file:', err);
+            res.status(500).send('Server error');
+        }
+    });
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
-import dotenv from 'dotenv';
-import connectDb from './config/dbConfig';
+import dotenv from "dotenv";
+import connectDb from "./config/dbConfig";
+import express from "express";
+import cors from "cors";
 
+import testRoutes from "./routes/testRoutes";
+import userRoutes from './routes/userRoutes';
+
+const app = express();
 dotenv.config();
+
 connectDb();
 
-console.log("Welcome to Saaya 25");
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use("/test", testRoutes);
+app.use("/user", userRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
